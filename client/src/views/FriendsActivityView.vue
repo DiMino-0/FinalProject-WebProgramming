a5741125448ff5ad1cf7cfb76f763df5d27af6d2
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-03-01 20:58:02 -0500

    added some basic content

--- client/src/views/FriendsActivityView.vue
+++ client/src/views/FriendsActivityView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div>This is where you would see your friends' activity</div>
 </template>
 
 <style scoped></style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main></main>
+  <main>
+    <section class="HomeView body-container">
+      <div class="container has-text-black">
+        <h1 class="title is-1" style="color: black">Welcome to the Fitness Tracker</h1>
+        <subtitle>
+          <p>
+            This is a simple activity tracker that allows you to log your workouts and see what your
+            friends are up to.
+          </p>
+        </subtitle>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
