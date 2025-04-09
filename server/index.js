52f3ba3714e7729aa22272682636e81589996e5b
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-04-09 18:49:05 -0400

    static file serving

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const { statusCodes } = require("./models/errors");
 const app = express();
+//TODO: Use the port our hosting provides
 const PORT = 8000;
 
+// Middleware
+app.use(express.json());
+
+app.use("/", express.static("dist"));
+//TODO: controller for posts
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+
+  const error = {
+    status,
+    message: err.message || statusCodes.INTERNAL_SERVER_ERROR,
+  };
+  res.status(status).send(error);
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
