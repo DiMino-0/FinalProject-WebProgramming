335f3274857e936c5bd701b35fbc5c722218f19e
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-04-17 01:43:43 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const dummyController = require("./controllers/DummyTable");
 const PORT = process.env.PORT ?? 8000;
 
 const app = express();
+
 // Middleware
 app.use(express.json());
-
 app.use("/api/v1/dummy", dummyController).use("/", express.static("dist"));
+
 //TODO: note, controller for Users (maybe tie into admin page user view) and Posts (Viewing your own vs friends posts may look different)
 //TODO: each controller should be able to access the database and perform CRUD operations
+
 //error handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
