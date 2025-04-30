0d137fbb66045c0f7cc10c73e885c80956bc1b0b
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2025-04-30 17:31:37 -0400

    formatting

--- client/src/components/LoginButton.vue
+++ client/src/components/LoginButton.vue
@@ -17,9 +17,21 @@ getAll().then((response) => {
     <div class="navbar-item has-dropdown is-hoverable">
       <a class="button is-light"> Log in </a>
 
-      <div class="navbar-dropdown">
-        <a class="navbar-item" v-for="user in users" :key="user.id" @click="login(user.id)">
-          {{ user.username }} {{ user.email }}
+      <div class="navbar-dropdown dropdown">
+        <a
+          class="navbar-item user-item"
+          v-for="user in users"
+          :key="user.id"
+          @click="login(user.id)"
+        >
+          <figure class="image is-24x24 mr-2">
+            <img
+              class="is-rounded"
+              :src="user.pfp_image_url || 'https://bulma.io/images/placeholders/24x24.png'"
+              :alt="`${user.username}'s profile`"
+            />
+          </figure>
+          <span>{{ user.username }} {{ user.email }}</span>
         </a>
       </div>
     </div>
@@ -39,10 +51,35 @@ getAll().then((response) => {
   overflow-y: auto;
 }
 
+/* Add these rules to properly position the dropdown */
+.navbar-item.has-dropdown.is-hoverable {
+  position: relative;
+}
+
+.navbar-item.has-dropdown.is-hoverable .navbar-dropdown {
+  margin-top: 0.05rem !important;
+  top: 100% !important;
+}
+
 .profile {
   display: flex;
   align-items: center;
   gap: 0.5em;
   font-size: small;
+}
+
+.user-item {
+  display: flex;
+  align-items: center;
+}
+
+.image.is-24x24 {
+  min-width: 24px;
+}
+
+.image.is-24x24 img {
+  height: 24px;
+  width: 24px;
+  object-fit: cover;
 }
 </style>
